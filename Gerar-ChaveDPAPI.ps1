c37011429ea1a0c4259a6b444be1339c920a177c
PC Dev1 <desenvolvimento@meusistema.com.br>
2026-08-14 22:31:37 -0300

    Mover e aprimorar o script Gerar-ChaveDPAPI
    Movemos o arquivo Gerar-ChaveDPAPI.ps1 da raiz do repositório para src/ para iniciar a compilação para EXE, remova o arquivo raiz antigo e aprimore a experiência do usuário do script: esclareça as mensagens (Hardware/Usuário), mantenha a saída criptografada, adicione um prompt para copiar a string DPAPI gerada para a área de transferência com feedback de sucesso/ignorar e uma pausa final. Adicionamos também o arquivo src/ms_gendpapi.ico como um recurso de ícone.

--- Gerar-ChaveDPAPI.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "=== EMBRULHAR CHAVES (DPAPI) ===" -ForegroundColor Cyan
-Write-Host "A chave gerada será associada ao Hardware deste servidor.`n" -ForegroundColor Yellow
-$SecretKey = Read-Host "COLE A KEY AQUI (TEXTO PLANO, SEM ESPAÇOS)" -AsSecureString
-$EncryptedString = ConvertFrom-SecureString -SecureString $SecretKey
-
-Write-Host "`n[OK] Sucesso! Copie a string abaixo e salve no config correspondente. Atente-se ao copiar corretamente:`n" -ForegroundColor Green
-Write-Host $EncryptedString -ForegroundColor Magenta
-Write-Host "`n======================================================================" -ForegroundColor Cyan
---
+++ src/Gerar-ChaveDPAPI.ps1
@@ -0,0 +1,23 @@
+Write-Host "=== EMBRULHAR CHAVES (DPAPI) ===" -ForegroundColor Cyan
+Write-Host "A chave gerada será associada ao Hardware/Usuário deste servidor.`n" -ForegroundColor Yellow
+
+$SecretKey = Read-Host "COLE A KEY AQUI (TEXTO PLANO, SEM ESPAÇOS)" -AsSecureString
+$EncryptedString = ConvertFrom-SecureString -SecureString $SecretKey
+
+Write-Host "`n[OK] Sucesso! A string foi gerada. Atente-se ao copiar corretamente:`n" -ForegroundColor Green
+Write-Host $EncryptedString -ForegroundColor Magenta
+Write-Host "`n======================================================================" -ForegroundColor Cyan
+
+$resposta = Read-Host "`nDeseja copiar o código para a área de transferência? (S/N)"
+
+if ($resposta -match '^[sS]') {
+    Set-Clipboard -Value $EncryptedString
+    Write-Host "`n[+] Código copiado para a área de transferência com sucesso!" -ForegroundColor Green
+} else {
+    Write-Host "`n[-] O código não foi copiado." -ForegroundColor DarkGray
+}
+
+Write-Host "`n======================================================================" -ForegroundColor Cyan
+
+
+Read-Host "Pressione ENTER para fechar a janela..."
