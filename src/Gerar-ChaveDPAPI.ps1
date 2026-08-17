f29a7e6a3e330132d21a03fc30fe76f6d2dd7657
PC Dev1 <desenvolvimento@meusistema.com.br>
2026-08-16 22:43:57 -0300

    Move o executável para a raiz; atualize o script do PowerShell e o ícone.
    Move o arquivo Gerar-ChaveDPAPI.exe da pasta src para a raiz do repositório. Atualizamos o arquivo src/Gerar-ChaveDPAPI.ps1 para esclarecer as mensagens para usuários em português (texto de prompt, confirmações de criptografia/área de transferência) e remova um caractere semelhante a uma BOM. Remova o antigo arquivo ms_gendpapi.ico e adicione um novo ícone em src/res/icon_ExeDPAPI.ico. Não há alterações comportamentais na lógica de criptografia — as alterações se referem às mensagens e à organização dos arquivos. Versão v1.4.7

--- src/Gerar-ChaveDPAPI.ps1
+++ src/Gerar-ChaveDPAPI.ps1
@@ -1,21 +1,22 @@
-﻿[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-Write-Host "=== EMBRULHAR CHAVES (DPAPI) ===" -ForegroundColor Cyan
-Write-Host "A chave gerada será associada ao Hardware/Usuário deste servidor.`n" -ForegroundColor Yellow
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-$SecretKey = Read-Host "COLE A KEY AQUI (TEXTO PLANO, SEM ESPAÇOS)" -AsSecureString
+Write-Host "=== EMBRULHAR CHAVES (DPAPI) ===" -ForegroundColor Cyan
+Write-Host "A chave gerada sera associada ao Hardware/Usuario deste dispositivo.`nA abertura do embrulho de keys so sera aberta neste mesmo dispositivo.`n`nPara mais informacoes, entre em contato conosco: contato[arroba]meusistema.com.br`n`n`n" -ForegroundColor Yellow
+
+$SecretKey = Read-Host "COLE A KEY AQUI QUE DESEJA ENCRIPTAR (TEXTO PLANO, SEM ESPACOS)" -AsSecureString
 $EncryptedString = ConvertFrom-SecureString -SecureString $SecretKey
 
-Write-Host "`n[OK] Sucesso! A string foi gerada. Atente-se ao copiar corretamente:`n" -ForegroundColor Green
+Write-Host "`n[OK] Sucesso! A string criptografada foi gerada. Atente-se ao copiar corretamente:`n" -ForegroundColor Green
 Write-Host $EncryptedString -ForegroundColor Magenta
 Write-Host "`n======================================================================" -ForegroundColor Cyan
 
-$resposta = Read-Host "`nDeseja copiar o código para a área de transferência? (S/N)"
+$resposta = Read-Host "`nDeseja copiar a string criptografada para a Area de transferencia? (S/N + enter)"
 
 if ($resposta -match '^[sS]') {
     Set-Clipboard -Value $EncryptedString
-    Write-Host "`n[+] Código copiado para a área de transferência com sucesso!" -ForegroundColor Green
+    Write-Host "`n[+] String copiada para a �rea de transferencia com sucesso! Cole no local correspondente no arquivo config.json" -ForegroundColor Green
 } else {
-    Write-Host "`n[-] O código não foi copiado." -ForegroundColor DarkGray
+    Write-Host "`n[-] A string nao foi copiada. O programa esta encerrado." -ForegroundColor DarkGray
 }
 
 Write-Host "`n======================================================================" -ForegroundColor Cyan
